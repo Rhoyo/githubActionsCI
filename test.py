999ad65af128c4541d8186b277bf0b356b356aeb
Ryan Ho <hor@flip2.engr.oregonstate.edu>
2021-06-06 23:38:30 -0700

    fixed more lints, added sub method to example.py

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 import example
 import unittest
 
+
 class testCases(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(example.add(1,2), 3)
+        self.assertEqual(example.add(1, 2), 3)
 
     def test_sub(self):
-        self.assertEqual(example.sub(3,2), 1)
+        self.assertEqual(example.sub(3, 2), 1)
 
     if __name__ == '__main__':
         unittest.main()
