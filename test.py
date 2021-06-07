fbb1a09ea4b7294d4c98b11380374f8a2f728ec8
Ryan Ho <hor@flip2.engr.oregonstate.edu>
2021-06-06 23:18:11 -0700

    added tests

---
+++ test.py
@@ -0,0 +1,10 @@
+import example
+import unittest
+
+class testCases(unittest.TestCase):
+
+    def test_add(self):
+	    self.assertEqual(example.add(1,2), 3)
+
+	if _name_ == '_main_':
+		unittest.main()
